c565f76139a7f8ebd25cd313fc25efcb1cf79bdf
An Vo <vothaian97@gmail.com>
2024-11-21 22:02:30 +0700

    Bài 18 - Page Factory

--- src/main/java/com/anhtester/keywords/WebUI.java
+++ src/main/java/com/anhtester/keywords/WebUI.java
@@ -2,6 +2,7 @@ package com.anhtester.keywords;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -32,6 +33,7 @@ public class WebUI {
         System.out.println("Set text " + text + " on element " + by);
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         wait.until(ExpectedConditions.visibilityOfElementLocated(by));
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(by)));
         driver.findElement(by).sendKeys(text);
     }
 
