783b41dfbaff4a33f2a39ed79b956396878fc049
An Vo <thaian.it15@gmail.com>
2025-02-12 22:38:27 +0700

    Update Selenium 4.28.1

--- src/test/java/com/anhtester/common/BaseTest.java
+++ src/test/java/com/anhtester/common/BaseTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
 import java.time.Duration;
@@ -16,7 +17,7 @@ public class BaseTest {
 
     @BeforeMethod
     @Parameters({"browser"})
-    public void createDriver(String browser) {
+    public void createDriver(@Optional("chrome") String browser) {
 
         switch (browser){
             case "chrome":
